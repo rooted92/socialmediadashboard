4e7130d9e6758db3193f92bb5beb5bceb82cd99f
Pedro Castaneda <petecast76@gmail.com>
2023-03-17 15:42:24 -0700

    Added Peer Review

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // Pedro Castaneda
 // 03-15-2023
 // Description: In this assignment we are taking the design from fronendmentor.io and with our current skillset in React we are to build this out for both desktop and mobile.
-// Peer Review by: 
+// Peer Review by Amar Mann: Really good work. Website looks pretty much spot on to the prototype we were given. Good attention to detail and solving for things like the toggle being reversed, the IG border being rounded while a gradient, and the rounded corners on the background rectangle. Responsiveness is perfect and how I'd imagine a production website to be at every width level from desktop to mobile. Dark mode is seamless and matches the prototype. Same with the hover effects
+// On the code side, really good use of props to make your components super versatile. And great use of css variables to make your dark mode implementation super efficient and easy to follow. Big fan of all the built-in bootstrap classes you used, seems like you're getting Bootstrap down really well. Really good use of the built in bootstrap classes to handle most of the responsiveness. The classNames you used for the columns work perfectly well but Bootstrap for React does have that alternative formatting you may want to look into. Not sure if it matters entirely which one use 
 
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -35,12 +35,12 @@ const Dashboard = () => {
                 <Row className='mb-4'>
                     {/* <Col className='col-2 '></Col> */}
                     <Col className='col-md-8 col-xs-8 col-sm-8 col-12 d-flex flex-column align-items-start'>
-                        <p className='title m-0'>Social Media Dashboard</p>
+                        <p className='title text-truncate m-0'>Social Media Dashboard</p>
                         <p className='m-0 bold'>Total Followers: 23,004</p>
                     </Col>
                     {/* <Col className='col-2'></Col> */}
                     <Col className='col-12 d-md-none'>
-                    <div className='lineMobile '></div>
+                        <div className='lineMobile'></div>
                     </Col>
                     <Col className='col-md-4 col-12 d-flex flex-row justify-content-between justify-content-md-end align-self-md-end'>
                         <p className='mt-2 mx-0 mx-md-3 pt-1 bold'>Dark Mode</p>
@@ -52,65 +52,65 @@ const Dashboard = () => {
                     {/* <Col className='col-2 '></Col> */}
                     <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-xl-0'>
                         <SocialMediaCard
-                        txtColor={'greenTxt'}
-                        smallNumbers={12}
-                        arrowImg={IconUp}
-                        supporterType={'followers'}
-                        numOfFollowers={1987}
-                        handle={'@nathanf'}
-                        logo={facebookLogo}
-                        socialMediaCard={'facebookDiv'} />
+                            txtColor={'greenTxt'}
+                            smallNumbers={12}
+                            arrowImg={IconUp}
+                            supporterType={'followers'}
+                            numOfFollowers={1987}
+                            handle={'@nathanf'}
+                            logo={facebookLogo}
+                            socialMediaCard={'facebookDiv'} />
                     </Col>
                     <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-xl-0'>
                         <SocialMediaCard
-                        txtColor={'greenTxt'}
-                        smallNumbers={99}
-                        arrowImg={IconUp}
-                        supporterType={'followers'}
-                        numOfFollowers={1044}
-                        handle={'@nathanf'}
-                        logo={twitterLogo}
-                        socialMediaCard={'twitterDiv'} />
+                            txtColor={'greenTxt'}
+                            smallNumbers={99}
+                            arrowImg={IconUp}
+                            supporterType={'followers'}
+                            numOfFollowers={1044}
+                            handle={'@nathanf'}
+                            logo={twitterLogo}
+                            socialMediaCard={'twitterDiv'} />
                     </Col>
                     <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4'>
                         <SocialMediaCard
-                        txtColor={'greenTxt'}
-                        smallNumbers={1099}
-                        arrowImg={IconUp}
-                        supporterType={'followers'}
-                        numOfFollowers={`${11}k`}
-                        handle={'@realnathanf'}
-                        logo={instagramLogo}
-                        divColor={'instaDiv'}
-                        socialMediaCard={'instagramDiv'} />
+                            txtColor={'greenTxt'}
+                            smallNumbers={1099}
+                            arrowImg={IconUp}
+                            supporterType={'followers'}
+                            numOfFollowers={`${11}k`}
+                            handle={'@realnathanf'}
+                            logo={instagramLogo}
+                            divColor={'instaDiv'}
+                            socialMediaCard={'instagramDiv'} />
                     </Col>
                     <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-lg-0'>
                         <SocialMediaCard
-                        txtColor={'redTxt'}
-                        smallNumbers={144}
-                        arrowImg={IconDown}
-                        supporterType={'subscribers'}
-                        numOfFollowers={8239}
-                        handle={'Nathan F.'}
-                        logo={youtubeLogo}
-                        socialMediaCard={'youtubeDiv'} />
+                            txtColor={'redTxt'}
+                            smallNumbers={144}
+                            arrowImg={IconDown}
+                            supporterType={'subscribers'}
+                            numOfFollowers={8239}
+                            handle={'Nathan F.'}
+                            logo={youtubeLogo}
+                            socialMediaCard={'youtubeDiv'} />
                     </Col>
                     {/* <Col className='col-2 '></Col> */}
                 </Row>
                 {/* <Container className='mt-5'> */}
-                    <Row className='mt-4'>
-                        {/* <Col className='col-2'></Col> */}
-                        <Col className='col-12 d-flex flex-column align-items-start'>
-                            <p className='overviewTxt'>Overview - Today</p>
-                        </Col>
-                        {/* <Col className='col-2'></Col> */}
-                    </Row>
+                <Row className='mt-4'>
+                    {/* <Col className='col-2'></Col> */}
+                    <Col className='col-12 d-flex flex-column align-items-start'>
+                        <p className='overviewTxt'>Overview - Today</p>
+                    </Col>
+                    {/* <Col className='col-2'></Col> */}
+                </Row>
                 {/* </Container> */}
                 {/* <Container> */}
-                    <Row>
-                        {/* <Col className='col-2 mx-2'></Col> */}
-                        <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-xl-0'>
-                            <OverviewCard
+                <Row>
+                    {/* <Col className='col-2 mx-2'></Col> */}
+                    <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-xl-0'>
+                        <OverviewCard
                             statDataType={'Page Views'}
                             logo={facebookLogo}
                             statDataNumbers={87}
@@ -118,9 +118,9 @@ const Dashboard = () => {
                             txtColor={'greenTxt'}
                             percentage={3}
                             overviewCard={'overviewCard'} />
-                        </Col>
-                        <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-lg-0'>
-                            <OverviewCard
+                    </Col>
+                    <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-lg-0'>
+                        <OverviewCard
                             statDataType={'Likes'}
                             logo={facebookLogo}
                             statDataNumbers={52}
@@ -128,9 +128,9 @@ const Dashboard = () => {
                             txtColor={'redTxt'}
                             percentage={2}
                             overviewCard={'overviewCard'} />
-                        </Col>
-                        <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-md-0'>
-                            <OverviewCard
+                    </Col>
+                    <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-md-0'>
+                        <OverviewCard
                             statDataType={'Likes'}
                             logo={instagramLogo}
                             statDataNumbers={5462}
@@ -138,9 +138,9 @@ const Dashboard = () => {
                             txtColor={'greenTxt'}
                             percentage={2257}
                             overviewCard={'overviewCard'} />
-                        </Col>
-                        <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-lg-0'>
-                            <OverviewCard
+                    </Col>
+                    <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-lg-0'>
+                        <OverviewCard
                             statDataType={'Profile Views'}
                             logo={instagramLogo}
                             statDataNumbers={52 + 'k'}
@@ -148,14 +148,14 @@ const Dashboard = () => {
                             txtColor={'greenTxt'}
                             percentage={1375}
                             overviewCard={'overviewCard'} />
-                        </Col>
-                        {/* <Col className='col-2 mx-2'></Col> */}
-                    </Row>
-                    {/* Second row of overview cards */}
-                    <Row className='mt-4 mt-md-1 mt-lg-4'>
-                        {/* <Col className='col-2 mx-2'></Col> */}
-                        <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-xl-0'>
-                            <OverviewCard
+                    </Col>
+                    {/* <Col className='col-2 mx-2'></Col> */}
+                </Row>
+                {/* Second row of overview cards */}
+                <Row className='mt-4 mt-md-1 mt-lg-4'>
+                    {/* <Col className='col-2 mx-2'></Col> */}
+                    <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-xl-0'>
+                        <OverviewCard
                             statDataType={'Retweets'}
                             logo={twitterLogo}
                             statDataNumbers={117}
@@ -163,9 +163,9 @@ const Dashboard = () => {
                             txtColor={'greenTxt'}
                             percentage={303}
                             overviewCard={'overviewCard'} />
-                        </Col>
-                        <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-lg-0'>
-                            <OverviewCard
+                    </Col>
+                    <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-lg-0'>
+                        <OverviewCard
                             statDataType={'Likes'}
                             logo={twitterLogo}
                             statDataNumbers={507}
@@ -173,9 +173,9 @@ const Dashboard = () => {
                             txtColor={'greenTxt'}
                             percentage={553}
                             overviewCard={'overviewCard'} />
-                        </Col>
-                        <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-lg-0'>
-                            <OverviewCard
+                    </Col>
+                    <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-lg-0'>
+                        <OverviewCard
                             statDataType={'Likes'}
                             logo={youtubeLogo}
                             statDataNumbers={107}
@@ -183,9 +183,9 @@ const Dashboard = () => {
                             txtColor={'redTxt'}
                             percentage={19}
                             overviewCard={'overviewCard'} />
-                        </Col>
-                        <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-lg-0'>
-                            <OverviewCard
+                    </Col>
+                    <Col className='col-xl-3 col-lg-6 col-12 col-md-6 mb-4 mb-lg-0'>
+                        <OverviewCard
                             statDataType={'Total Views'}
                             logo={youtubeLogo}
                             statDataNumbers={1407}
@@ -193,8 +193,8 @@ const Dashboard = () => {
                             txtColor={'redTxt'}
                             percentage={12}
                             overviewCard={'overviewCard'} />
-                        </Col>
-                    </Row>
+                    </Col>
+                </Row>
                 {/* </Container> */}
             </Container>
         </>
